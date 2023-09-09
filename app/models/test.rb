38630c7503953269b34fab93838be9aa499aa200
Anton Goroshko <radjahair@gmail.com>
2023-09-09 21:56:32 +1000

    feat: add Test.titles_by_category

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,4 +2,12 @@ class Test < ApplicationRecord
   belongs_to :category
   has_and_belongs_to_many :users
   has_many :questions
+
+  def self.titles_by_category(title)
+    return self.joins(:category)
+               .where(category: {title: title})
+               .order(title: :desc)
+               .select(:title)
+               .map(&:title)
+  end
 end
