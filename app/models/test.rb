0e91942d66dc495daa4d36a42bfc7caab8df5583
Anton Goroshko <radjahair@gmail.com>
2023-09-13 13:54:34 +1000

    feat: add scopes

--- app/models/test.rb
+++ app/models/test.rb
@@ -5,10 +5,13 @@ class Test < ApplicationRecord
   has_many :tests_users
   has_many :users, through: :tests_users
 
-  def self.titles_by_category(title)
-    self.joins(:category)
-        .where(category: {title: title})
-        .order(title: :desc)
-        .pluck(:title)
+  scope :easy, -> { where(level: 0..1) }
+  scope :middle, -> { where(level: 2..4) }
+  scope :hard, -> { where(level: 5..) }
+  scope :titles_by_category, -> (title) do
+    joins(:category)
+      .where(category: {title: title})
+      .order(title: :desc)
+      .pluck(:title)
   end
 end
