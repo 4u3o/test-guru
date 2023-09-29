53ae87a2c8d8168d30811f4be2799c266f267bb3
Anton Goroshko <radjahair@gmail.com>
2023-09-29 14:34:24 +1000

    feat: add user.email validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   has_many :test_passages
   has_many :tests, through: :test_passages, inverse_of: 'users'
 
+  validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+
   has_secure_password
 
   def tests_by_level(level)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_09_27_234135) do
+ActiveRecord::Schema.define(version: 2023_09_29_043301) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 2023_09_27_234135) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "email", null: false
     t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "answers", "questions"
