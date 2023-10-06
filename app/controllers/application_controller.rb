3c5e82b1df7d6fe048b7dfb9174e062a8892fd56
Anton Goroshko <radjahair@gmail.com>
2023-10-07 07:11:00 +1000

    refactor: add User#admin?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   private
 
     def after_sign_in_path_for(user)
-      user.is_a?(Admin) ? admin_tests_path : root_path
+      user.admin? ? admin_tests_path : root_path
     end
 end
