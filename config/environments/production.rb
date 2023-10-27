620e41af9bbf13aa01b0dd8410b8102707b2001f
Anton Goroshko <radjahair@gmail.com>
2023-10-27 13:51:14 +1000

    fix: mail sending

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.default_url_options = {
-    host: 'test-guru-eoi6.onrender.com.'
+    host: 'test-guru-eoi6.onrender.com'
   }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
