04cbf411e8722fd21a1042b0e1c9936bbf9902c7
Anton Goroshko <radjahair@gmail.com>
2023-10-27 15:10:41 +1000

    fix: mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,16 +67,19 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.default_url_options = {
-    host: 'https://test-guru-eoi6.onrender.com'
+    host: 'test-guru-eoi6.onrender.com'
   }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
+    domain: 'gmail.com',
     user_name: ENV['SMTP_USERNAME'],
     password: ENV['SMTP_PASSWORD'],
-    authentication: 'plain',
-    enable_starttls_auto: true
+    authentication: :plain,
+    enable_starttls: true,
+    open_timeout: 5,
+    read_timeout: 5
   }
 
   # Ignore bad email addresses and do not raise email delivery errors.
