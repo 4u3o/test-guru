b0dbc379c6654f7e07e4684f7b72d85665b1f8cd
Anton Goroshko <radjahair@gmail.com>
2023-10-27 14:49:50 +1000

    fix: action_mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,11 +67,11 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.default_url_options = {
-    host: 'test-guru-eoi6.onrender.com'
+    host: 'https://test-guru-eoi6.onrender.com'
   }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    adress: 'smtp.gmail.com',
+    address: 'smtp.gmail.com',
     port: 587,
     user_name: ENV['SMTP_USERNAME'],
     password: ENV['SMTP_PASSWORD'],
